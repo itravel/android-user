941ff9427283c2422d1e6d8c67b587f8ced5b1a7
jinwb000 <jinwb000@sina.com>
2014-03-20 21:43:49 +0800

    adjust UI style according to design

--- demo/src/demo/travel/views/explore/DestActivity.java
+++ demo/src/demo/travel/views/explore/DestActivity.java
@@ -1,25 +1,51 @@
 package demo.travel.views.explore;
 
-
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import demo.travel.R;
-
-
 
 public class DestActivity extends FragmentActivity {
 	@Override
-	public void onCreate(Bundle savedInstanceState){
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_explore_destination);
+		
+		Fragment fragment = new DestMapFragment();
+		getSupportFragmentManager().beginTransaction().add(R.id.activity_explore_destination, fragment).commit();
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();    
-	    inflater.inflate(R.menu.menu_explore_sub_actions, menu);    
-	    return super.onCreateOptionsMenu(menu);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_explore_sub_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_switch_list:
+			switch2list();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void switch2list() {
+		Fragment newFragment = new DestListFragment();
+		Bundle args = new Bundle();
+		args.putInt("hello", 0);
+		newFragment.setArguments(args);
+		FragmentTransaction transaction = getSupportFragmentManager()
+				.beginTransaction();
+		transaction.replace(R.id.activity_explore_destination, newFragment);
+		transaction.addToBackStack(null);
+		transaction.commit();
 	}
 }
--- demo/src/demo/travel/views/explore/DestListFragment.java
+++ demo/src/demo/travel/views/explore/DestListFragment.java
@@ -1,14 +1,14 @@
 package demo.travel.views.explore;
 
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import demo.travel.R;
 
-public class DestListFragment extends ListFragment {
+public class DestListFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
