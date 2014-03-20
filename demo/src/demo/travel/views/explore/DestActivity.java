a4cac529f016be6193cb15a83846875453d4c8fd
jinwb000 <jinwb000@sina.com>
2014-03-20 11:35:33 +0800

    push ommitted files in last ci

--- demo/src/demo/travel/views/explore/DestActivity.java
+++ demo/src/demo/travel/views/explore/DestActivity.java
@@ -3,6 +3,8 @@ package demo.travel.views.explore;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import demo.travel.R;
 
 
@@ -12,5 +14,12 @@ public class DestActivity extends FragmentActivity {
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_explore_destination);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();    
+	    inflater.inflate(R.menu.menu_explore_sub_actions, menu);    
+	    return super.onCreateOptionsMenu(menu);
 	}
 }
